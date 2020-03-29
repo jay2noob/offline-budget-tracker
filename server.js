775607dfdad1a73f2cd35db1d87522fbab53ee9d
John Fields <jjfields24@outlook.com>
2020-03-29 04:30:57 -0400

    fixing db connection

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 // routes
 app.use(require("./routes/api.js"));
